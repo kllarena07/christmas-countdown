2bd24e2ada5ac047237be35b5829abfac090108d
Kieran Llarena <baymax709@gmail.com>
2022-12-27 12:18:35 -0500

    added responsiveness

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,52 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  const calculateDaysFromChristmas = () => {
+    let today = new Date()
+    let target_year = today.getMonth() + 1 === 12 && today.getDate() > 25 ? today.getFullYear() + 1 : today.getFullYear()
+    let next_christmas = new Date(`12/25/${target_year}`)
+    let difference = next_christmas.getTime() - today.getTime()
+    return { difference: Math.ceil(difference / (1000 * 3600 * 24)), year: target_year }
+  }
+
+  let day_difference = calculateDaysFromChristmas().difference
+  let year = calculateDaysFromChristmas().year
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <section id="app-container">
+    <div id="mask-container">
+      <img src="./public/tree-color.png" alt="holiday" />
+    </div>
+    {#if day_difference ===0}
+      <h1>Merry Christmas!</h1>
+    {/if}
+    {#if day_difference !==0}
+      <p>There are only</p>
+      <p>{day_difference} days</p>
+      <p>till Christmas {year}!</p>
+  {/if}
+  </section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  main {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  #app-container {
+    text-align: center;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  #app-container p {
+    margin: 0;
+    font-size: clamp(2rem, 2vw, 3vw);
+    line-height: clamp(3rem, 2.5vw, 3vw);
   }
-  .read-the-docs {
-    color: #888;
+
+  #app-container img {
+    -webkit-user-drag: none;
   }
 </style>
